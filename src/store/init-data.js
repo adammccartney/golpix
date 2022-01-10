299534d68e70e467e97a46f6768487ee4a43b61e
Adam McCartney <mccartney@mdw.ac.at>
2022-01-10 13:19:24 +0100

    cleanup store

--- src/store/init-data.js
+++
@@ -1,7 +0,0 @@
-import { createCell } from '../modules/Cell.js';
-
-export const CELL = { x: 0, 
-                      y: 0,
-                      alive: false,
-                      neighbors: [],
-                      next_state: false};
